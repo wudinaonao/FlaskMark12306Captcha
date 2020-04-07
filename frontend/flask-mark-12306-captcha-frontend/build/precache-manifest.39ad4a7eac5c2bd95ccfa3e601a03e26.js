aa65aea08852470f333d90220b5fc2fcd5fb5869
wudinaonao <wudinaonao250@gmail.com>
2020-04-07 18:57:37 +0800

    first commit

---
+++ frontend/flask-mark-12306-captcha-frontend/build/precache-manifest.39ad4a7eac5c2bd95ccfa3e601a03e26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35eb819369ed0bb6fa913a7812ffc8ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f9afb4a692408480acda",
+    "url": "./static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "6ce66d6c02175ec4edbc",
+    "url": "./static/css/main.cdfcf7d8.chunk.css"
+  },
+  {
+    "revision": "f9afb4a692408480acda",
+    "url": "./static/js/2.385c1c95.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.385c1c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce66d6c02175ec4edbc",
+    "url": "./static/js/main.eb09729b.chunk.js"
+  },
+  {
+    "revision": "a63731602a9433806e10",
+    "url": "./static/js/runtime-main.3b5be632.js"
+  }
+]);
